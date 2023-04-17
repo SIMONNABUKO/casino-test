b1ae48792ac5db4c1ef0e387acc3d35d64b05cae
Simon Angatia <simonnabuko@gmail.com>
2023-04-17 15:18:37 +0300

    Testing rendering games in an iFrame

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -3,7 +3,6 @@
 <html>
 <head>
 	<title>Full-Screen iFrame Example</title>
-    <meta http-equiv="X-Frame-Options" content="ALLOW-FROM {{ url('https://test.rentalb.al/') }}">
 </head>
 <body>
 <iframe src="{{$gameData->url}}" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
